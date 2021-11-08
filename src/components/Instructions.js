98c5db36a0d01beb9b0602301f8683884919a50e
robert-otieno <robertotieno.or@gmail.com>
2021-11-08 10:24:31 +0300

    Creating Custom Components in React

---
+++ src/components/Instructions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import emoji from './emoji.svg'
+
+const Instructions = () => (
+  <div className="instructions">
+    <img alt="laughing crying emoji" src={emoji} />
+    <p>Click on an emoji to view the emoji short name.</p>
+  </div>
+)
+
+export default Instructions;
